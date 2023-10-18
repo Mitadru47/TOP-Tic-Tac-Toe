9ee6c2f3f0017de65fde2e2dd555aa0a3ef632e5
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2023-10-18 13:22:38 +0530

    Basic Layout & Script Updates

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-let html = document.querySelector("html");
-html.style.height = "100vh";
+// Module:
+const modules = (function (){
+
+    const gameBoard = (array) => { return {array} } ;
+    const displayController = (state) => { return {state} };
+
+    return { gameBoard, displayController };
+})();
+
+const board = modules.gameBoard(["X","O","X","O","X","O","X","O","X"]);
+// console.log(board);
+
+// Factory Function:
+const createPlayer = function (name, turn){
+    return { name, turn };
+}
+
+function renderGameboard(){
+
+    let gameBoardSlots = document.querySelectorAll(".gameBoard div");
+    gameBoardSlots.forEach((slot, key) => {
+
+        // console.log(key, slot); 
+        slot.innerText = board.array[key];
+    });
+}
+
+renderGameboard();
