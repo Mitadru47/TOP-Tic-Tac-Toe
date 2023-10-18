8d025d8a2587df64fec1f4f5011e0fee6fffc91f
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2023-10-18 15:04:10 +0530

    Player - Gameboard Interaction Support

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const board = modules.gameBoard(["X","O","X","O","X","O","X","O","X"]);
 // console.log(board);
 
 // Factory Function:
-const createPlayer = function (name, turn){
-    return { name, turn };
+const createPlayer = function (type){
+    return { type };
 }
 
 function renderGameboard(){
@@ -25,4 +25,71 @@ function renderGameboard(){
     });
 }
 
-renderGameboard();
+// renderGameboard();
+
+function handleInput(player1, player2){
+
+    let turnCounter = modules.displayController(1);
+    console.log(turnCounter);
+
+    let gameBoardSlots = document.querySelectorAll(".gameBoard div");
+    gameBoardSlots.forEach((slot) => {
+
+        slot.addEventListener("click", function (){
+
+            if((turnCounter.state % 2) === 0 && (slot.innerText === "")){
+
+                slot.innerText = player2.type;
+                turnCounter.state = turnCounter.state + 1;
+
+                console.log(turnCounter);
+
+            } else if((turnCounter.state % 2) !== 0 && (slot.innerText === "")){
+
+                slot.innerText = player1.type
+                turnCounter.state = turnCounter.state + 1;
+
+                console.log(turnCounter);
+            }
+
+            // console.log(slot);
+    
+        }, true);
+    });
+}
+
+function startGame(){
+
+    let player1 = {};
+    let player2 = {};
+
+    let xButton = document.querySelector("#X");
+    xButton.addEventListener("click", function (){
+
+        console.log("X Button Clicked!");
+
+        player1 = createPlayer("X");
+        player2 = createPlayer("O")
+
+        // console.log(player1, player2);
+        handleInput(player1, player2);
+
+    }, true);
+
+    let oButton = document.querySelector("#O");
+    oButton.addEventListener("click", function (){
+
+        console.log("O Button Clicked!");
+
+        player1 = createPlayer("O");
+        player2 = createPlayer("X");
+
+        console.log(player1, player2);
+
+        // console.log(player1, player2);
+        handleInput(player1, player2);
+
+    }, true);
+}
+
+startGame();
